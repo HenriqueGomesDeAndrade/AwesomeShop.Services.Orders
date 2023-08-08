3cc7dc85074748e70e6097bd506e7a649bf2292a
Henrique Gomes de Andrade <42363331+HenriqueGomesDeAndrade@users.noreply.github.com>
2023-08-07 22:29:16 -0300

    Add CQRS classes

---
+++ AwesomeShop.Services.Orders.Application/Extensions.cs
@@ -0,0 +1,21 @@
+﻿using AwesomeShop.Services.Orders.Application.Commands.Handler;
+using MediatR;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AwesomeShop.Services.Orders.Application
+{
+    public static class Extensions
+    {
+        public static IServiceCollection AddHandlers(this IServiceCollection services)
+        {
+            services.AddMediatR(typeof(AddOrderHandler).Assembly);
+
+            return services;
+        }
+    }
+}
